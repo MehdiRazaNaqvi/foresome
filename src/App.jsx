588d0ed876692e49ad34fce33a4e31e87b47c1f3
MehdiRazaNaqvi <mehdiraza1232016@gmail.com>
2022-12-24 23:33:11 +0500

    main screen done

---
+++ src/App.jsx
@@ -0,0 +1,171 @@
+//https://www.figma.com/file/h9B5qC5V4mIohxH2vzy8vO/playgolfwithfriends-Mock?node-id=0%3A1&t=KRWdK3dcXqIMz4x0-0
+
+import { Button, Form, FormGroup, Input, FormText } from 'reactstrap';
+
+import "./style/app.css"
+import "./style/screen3.css"
+import Logo from "./images/logo.png"
+import Download1 from "./images/download1.png"
+import Download2 from "./images/download2.png"
+import Mobile1 from "./images/mobile1.png"
+import Mobile2 from "./images/mobile4.svg"
+import Mobile3 from "./images/mobile3.svg"
+import Mobile4 from "./images/mobile4.png"
+
+import Hockey from "./images/hockey.svg"
+
+
+
+
+const App = () => {
+
+
+  return (
+
+
+    <div className="foresome_base">
+
+
+      <div className="foresome_home_navbar">
+
+        <img className="logo" src={Logo} />
+
+      
+
+        <div className="download_img_div download_img_screen_div" style={{ width: "min-content",height:"100%" }}>
+          <img className="download_img_screen" src={Download1} />
+          <img className="download_img_screen" src={Download2} />
+        </div>
+
+      </div>
+
+
+      <div className="foresome_screen1">
+
+        <div className="screen1_left">
+
+          <img src={Hockey} className="hockey_img" />
+
+          <h1 className="screen1_heading">Join local tee times and find your ideal golf partner anywhere</h1>
+          <span className="screen1_text">Golf's number one compatibility app that helps you find partners that you actually want to play with whereever you are.</span>
+
+          <div className="download_img_div download_img_screen_div">
+            <img className="download_img_screen" src={Download1} />
+            <img className="download_img_screen" src={Download2} />
+          </div>
+
+        </div>
+
+
+        <div className="screen1_right">
+
+          <img src={Mobile1} className="mobile1" />
+
+        </div>
+
+      </div>
+
+
+      <div className="foresome_screen2">
+
+
+        <div className="signup_div" style={{ marginBottom: "5rem" }}>
+
+          <span className="signup_left"><h3 className="screen2_heading">Sign up for the newsletter today.</h3> <span className="screen2_text">Get launch and be the first to know when Foresome is live in your area. Make the most of vacations, business trips, time off, or the weekend at home with Foresome, golf's only compatibility app</span> </span>
+
+
+
+          <span className="signup_right">
+            <Input className='screen2_input' style={{ backgroundColor: "transparent", color: "white" }} placeholder='Your Email' />
+            <Button color="light" style={{ color: "rgba(23, 107, 58, 1)" }}>Subscribe</Button>{' '}
+          </span>
+
+        </div>
+
+        <span className='page2_subheading'>HOW IT WORKS</span>
+        <span className='page2_heading'>Introducing Foresome</span>
+        <span className='page2_text' style={{ marginBottom: "3rem" }}>We all know that Tiger and Sergio hated playing with each other, but they had no other choice. You do! Foresome is the golf compatibility app that helps you find ideal golf partners, either at your local golf course or when you are on the road.</span>
+
+
+
+        <span className='split_div'>
+
+          <img src={Mobile2} style={{ width: "30%", height: "30rem" }} />
+
+          <span className='miniscreen1_text_div'>
+            <span className='page2_subheading' style={{ paddingBottom: "1rem" }}>FEATURE COMING SOON</span>
+            <span className='miniscreen1_heading'>Join Local tee Times</span>
+            <span className='page2_text' style={{ width: "100%", paddingBottom: "1.5rem" }}>Never play alone again, find your next golf partner on Foresome anytime anywhere</span>
+
+            <Button color="success" style={{ width: "70%" }}>Join Now</Button>
+
+          </span>
+
+        </span>
+
+
+
+
+        <span className='split_div'>
+
+
+          <span className='miniscreen1_text_div'>
+
+            <span className='miniscreen1_heading'>Find Golfers Anywhere</span>
+            <span className='page2_text' style={{ width: "100%", paddingBottom: "1.5rem" }}>Enter parameters such as GHIN, pace of play, gambling preference, dinking preference and more</span>
+
+            <Button color="success" style={{ width: "70%" }}>Join Now</Button>
+
+          </span>
+          <img src={Mobile3} style={{ width: "30%", height: "30rem" }} />
+
+
+        </span>
+
+      </div>
+
+
+      <div className="foresome_screen_3">
+
+        <img src={Mobile4} className="screen3_left" />
+        <span className="signup_left screen3_right">
+          <h3 className="screen2_heading">Make the most of your vacation with Foresome</h3>
+          <span className="screen2_text">We all have been on vacation with a significant other and found ourselves staring out the hotel window at a championship Get launch and be the first to know when Foresome is live in your area. Make the most of vacations, business trips, time off, or the weekend at home with Foresome, golf's only compatibility app</span>
+        </span>
+
+
+
+      </div>
+
+
+
+      <div className="foresome_screen_4">
+
+
+        <img src={Logo} className="footer_logo" />
+        <h3 className="screen2_heading footer_heading">Play golf on business trips with ease</h3>
+        <span className="footer_text screen1_text">We all have been on vacation with a significant other and found ourselves staring out the hotel window at a championship Get launch and be the first to know when Foresome is live in your area. Make the most of vacations, business trips, time off, or the weekend at home with Foresome, golf's only compatibility app</span>
+
+
+        <div className="download_img_div download_img_screen_div footer_btns">
+          <img className="download_img_screen" src={Download1} />
+          <img className="download_img_screen" src={Download2} />
+        </div>
+
+
+      </div>
+
+
+
+      <div className="foresome_footer">@Copyright 2022 Syed Mehdi Raza Naqvi. All Rights Reserved.</div>
+
+
+    </div>
+
+  )
+
+}
+
+
+
+export default App
